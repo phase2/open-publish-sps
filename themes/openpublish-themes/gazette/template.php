348f9066583af313f4226456a403fc76a0f6d220
David Coffey <dcoffey@phase2technology.com>
2012-06-11 18:37:10 -0400

    [#OPISH-336] IE8 - Author image on articles and blog posts does not show up

--- themes/openpublish-themes/gazette/template.php
+++ themes/openpublish-themes/gazette/template.php
@@ -10,6 +10,15 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+/**
+ * Implements hook_preprocess_html().
+ */
+function gazette_preprocess_html(&$vars) {
+  //add IE CSS
+  drupal_add_css(path_to_theme() . '/css/ie8.css', array('group' => 2001, 'weight' => 100, 'browsers' => array('IE' => 'lte IE 8', '!IE' => FALSE), 'preprocess' => FALSE));
+}
+
 function gazette_preprocess_views_view_unformatted(&$vars) {
   foreach($vars['classes'] as &$rowclasses) {
     $rowclasses[] = 'clearfix';
@@ -21,4 +30,3 @@ function gazette_preprocess_views_view_unformatted(&$vars) {
     $rowclasses .= ' clearfix';
   }
 }
- 
